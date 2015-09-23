c026be9212c1175de7188982a32d75a48bd527bf
Evan Lovely <evan@evanlovely.com>
2015-09-23 12:45:03 -0700

    solidifying options approach

--- app/index.js
+++ app/index.js
@@ -3,64 +3,90 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var exec = require('child_process').exec;
+var _ = require('lodash');
 
 var answers = {};
+var options = {
+
+};
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
+    if (!this.options.skipWelcome) {
+      // Have Yeoman greet the user.
+      this.log(yosay(
+        'Welcome to the remarkable ' + chalk.red('PatternLabStarter') + ' generator!' + '\nPlease be in the folder you want files in now.'
+      ));
+    }
+
     this.pkg = require('../package.json');
+
+    options.themeName = _.last(this.env.cwd.split('/'));
+    //console.log('---');
+    //console.log(options);
+    //console.log(this.options);
+    //console.log(this.config);
+    //console.log('---');
+    options = _.assign(options, this.options, this.config);
+    //console.log(options);
+    //console.log('---');
+
   },
 
   prompting: function () {
-    //var done = this.async();
+    var done = this.async();
+    var prompts = [];
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the remarkable ' + chalk.red('PatternLabStarter') + ' generator!' + '\nPlease be in the folder you want files in now.'
-    ));
+    //if (!options.themeName) {
+    //  prompts.push({
+    //    type: 'input',
+    //    name: 'themeName',
+    //    message: 'What would you like to call the theme?',
+    //    default: _.last(options.env.cwd.split('/'))
+    //  });
+    //}
 
-    var prompts = [
-      //{
-      //  type: 'confirm',
-      //  name: 'extras',
-      //  message: 'Would you like to install some extras too?',
-      //  default: false
-      //}
-      //{
-      //  type: 'confirm',
-      //  name: 'installDeps',
-      //  message: 'Want to install dependencies after?',
-      //  default: true
-      //}
-    ];
+    //if (!options.themePath) {
+    //  prompts.push({
+    //    name: 'themePath',
+    //    message: 'Where would you like to install it?',
+    //    default: ''
+    //  });
+    //}
 
-    //this.prompt(prompts, function (props) {
-    //  if (props.extras) {
-    //    this.composeWith('pattern-lab-starter:extras');
-    //  }
-    //  done();
-    //}.bind(this));
+    if (!options.installDeps) {
+      prompts.push({
+        type: 'confirm',
+        name: 'installDeps',
+        message: 'Want to install dependencies after?',
+        default: true
+      });
+    }
+
+    this.prompt(prompts, function (props) {
+      options = _.assign(options, props);
+      done();
+    }.bind(this));
   },
 
-  writing: function() {
-    var installPath = '';
-    if (this.options.themePath) {
-      installPath = this.options.themePath;
-    }
+  writing: function () {
+
     var done = this.async();
     this.remote('phase2', 'pattern-lab-starter', 'master', function (err, remote) {
-      remote.directory('.', installPath);
+      remote.directory('.', options.themePath);
       done();
     }, true);
   },
 
   install: function () {
-    this.installDependencies();
-    this.log('Installing Ruby dependencies with `bundle install` ... ');
-    this.spawnCommand('bundle', ['install']);
+    if (options.installDeps) {
+      this.installDependencies();
+      this.log('Installing Ruby dependencies with `bundle install` ... ');
+      this.spawnCommand('bundle', ['install']);
+    }
   },
 
-  end: function() {
+  end: function () {
     //var done = this.async();
     //var that = this;
 
