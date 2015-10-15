eb592ff6e8a601d9f91d522811d9ee79999d1695
Evan Lovely <evan@evanlovely.com>
2015-10-15 11:56:41 -0700

    Setting up prompts to be in line with GDT's

--- app/prompts.js
+++ app/prompts.js
@@ -2,9 +2,33 @@
 
 module.exports = [
   {
+    name: 'projectName',
+    message: 'Machine name of project?'
+  },
+  {
     name: 'themeName',
     message: 'What would you like to name the theme?',
     default: ''
+  },
+  {
+    name: 'drupalDistro',
+    message: 'What distribution of Drupal is installed?',
+    type: 'list',
+    default: 'drupal',
+    choices: [
+      'drupal',
+      'openatrium'
+    ]
+  },
+  {
+    name: 'drupalDistroVersion-drupal',
+    message: 'What version of Drupal is installed?',
+    type: 'list',
+    default: '7.x',
+    choices: [
+      '7.x',
+      '8.x'
+    ]
   },
   {
     name: 'projectDescription',
