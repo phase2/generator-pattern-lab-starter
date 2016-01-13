84f4df4445876a1f6fff46af07255b2297eead2d
Evan Lovely <evan@evanlovely.com>
2016-01-12 16:26:45 -0800

    fixing extras sub-gen

--- extras/index.js
+++ extras/index.js
@@ -2,57 +2,30 @@
 var generators = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var myPrompts = require('../app/prompts.js');
 var fs = require('fs');
 var _ = require('lodash');
-//var options = {};
 
 module.exports = generators.Base.extend({
   initializing: function () {
     //this.pkg = require('../package.json');
-    //options = _.assign(options, this.options);
   },
 
-  //prompting: function () {
-  //var done = this.async();
+  prompting: function () {
+    var done = this.async();
+    var prompts = [];
 
-  // Have Yeoman greet the user.
-  //this.log(yosay(
-  //  'Hi! Let\'s get you some extra goodies to use!'
-  //));
+    myPrompts.forEach(function (item) {
+      if (_.isUndefined(this.options[item.name])) {
+        prompts.push(item);
+      }
+    }.bind(this));
 
-  // Creates an array of directories inside "app/templates/" - our pattern options
-  //var patternOptions = _.filter(fs.readdirSync(this.templatePath()), function (value) {
-  //  // filtering out directories that start with `.` - like `.DS_Store`
-  //  if (!value.match(/^\./)) {
-  //    return true;
-  //  } else {
-  //    return false;
-  //  }
-  //});
-  ////
-  //if (patternOptions.drupal7 && options.backEnd === '7.x') {
-  //  options.patternCollection = 'drupal7';
-  //}
-
-  // Setting up our questions
-  //var prompts = [];
-
-  //if (_.isUndefined(options.patternCollection)) {
-  //  prompts.push({
-  //    type: 'list',
-  //    name: 'patternCollection',
-  //    message: 'Which set of patterns would you like?',
-  //    choices: patternOptions
-  //  });
-  //}
-
-  // Asking our questions
-  //this.prompt(prompts, function (props) {
-  //  options = _.assign(options, props);
-  //  done();
-  //}.bind(this));
-
-  //},
+    this.prompt(prompts, function (props) {
+      this.options = _.assign(this.options, props);
+      done();
+    }.bind(this));
+  },
 
   writing: {
     app: function () {
@@ -60,14 +33,14 @@ module.exports = generators.Base.extend({
       var destPath = this.options.themePath || './';
 
       if (this.options.drupalDistro === 'drupal') {
-        if (this.options['drupalDistroVersion-drupal'] === '7.x') {
+        if (this.options['drupalDistroVersion'] === '7.x') {
           patternCollection = 'drupal-7.x';
         }
-        if (this.options['drupalDistroVersion-drupal'] === '8.x') {
+        if (this.options['drupalDistroVersion'] === '8.x') {
           patternCollection = 'drupal-8.x';
         }
       } else if (this.options.drupalDistro === 'openatrium') {
-        if (this.options['drupalDistroVersion-openatrium'] === '7.x') {
+        if (this.options['drupalDistroVersion'] === '7.x') {
           patternCollection = 'openatrium-7.x';
         }
       }
@@ -81,13 +54,13 @@ module.exports = generators.Base.extend({
         );
 
         if (this.options.drupalDistro === 'drupal') {
-          if (this.options['drupalDistroVersion-drupal'] === '7.x') {
+          if (this.options['drupalDistroVersion'] === '7.x') {
             this.fs.move(
               this.destinationPath(destPath) + '/' + 'pattern_lab_starter.info',
               this.destinationPath(destPath) + '/' + this.options.themeName + '.info'
             );
           }
-          if (this.options['drupalDistroVersion-drupal'] === '8.x') {
+          if (this.options['drupalDistroVersion'] === '8.x') {
             this.fs.move(
               this.destinationPath(destPath) + '/' + 'pattern_lab_starter.info.yml',
               this.destinationPath(destPath) + '/' + this.options.themeName + '.info.yml'
@@ -98,11 +71,14 @@ module.exports = generators.Base.extend({
             );
           }
         } else if (this.options.drupalDistro === 'openatrium') {
-          if (this.options['drupalDistroVersion-openatrium'] === '7.x') {
+          if (this.options['drupalDistroVersion'] === '7.x') {
             this.fs.move(
               this.destinationPath(destPath) + '/' + 'pattern_lab_starter.info',
               this.destinationPath(destPath) + '/' + this.options.themeName + '.info'
             );
+          }
+          if (this.options['drupalDistroVersion'] === '8.x') {
+            console.log('Nothing exists for OpenAtrium on Drupal 8.x yet');
           }
         }
 
