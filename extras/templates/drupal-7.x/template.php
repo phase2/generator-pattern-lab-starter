c0126518c6deb53e7d6bfb686e53719b637a7f8e
Evan Lovely <evan@evanlovely.com>
2016-03-05 18:53:59 -0800

    Adding function to remove default Drupal CSS

--- extras/templates/drupal-7.x/template.php
+++ extras/templates/drupal-7.x/template.php
@@ -1,4 +1,49 @@
 <?php
+
+/**
+  * Implements hook_css_alter().
+  */
+function <%= themeName %>_css_alter(&$css) {
+  // Remove Drupal core css
+  // $exclude = array(
+  //   'modules/aggregator/aggregator.css' => FALSE,
+  //   'modules/block/block.css' => FALSE,
+  //   'modules/book/book.css' => FALSE,
+  //   'modules/comment/comment.css' => FALSE,
+  //   'modules/dblog/dblog.css' => FALSE,
+  //   'modules/field/theme/field.css' => FALSE,
+  //   'modules/file/file.css' => FALSE,
+  //   'modules/filter/filter.css' => FALSE,
+  //   'modules/forum/forum.css' => FALSE,
+  //   'modules/help/help.css' => FALSE,
+  //   'modules/menu/menu.css' => FALSE,
+  //   'modules/node/node.css' => FALSE,
+  //   'modules/openid/openid.css' => FALSE,
+  //   'modules/poll/poll.css' => FALSE,
+  //   'modules/profile/profile.css' => FALSE,
+  //   'modules/search/search.css' => FALSE,
+  //   'modules/statistics/statistics.css' => FALSE,
+  //   'modules/syslog/syslog.css' => FALSE,
+  //   'modules/system/admin.css' => FALSE,
+  //   'modules/system/maintenance.css' => FALSE,
+  //   'modules/system/system.css' => FALSE,
+  //   'modules/system/system.admin.css' => FALSE,
+  //   'modules/system/system.base.css' => FALSE,
+  //   'modules/system/system.maintenance.css' => FALSE,
+  //   'modules/system/system.messages.css' => FALSE,
+  //   'modules/system/system.menus.css' => FALSE,
+  //   'modules/system/system.theme.css' => FALSE,
+  //   'modules/taxonomy/taxonomy.css' => FALSE,
+  //   'modules/tracker/tracker.css' => FALSE,
+  //   'modules/update/update.css' => FALSE,
+  //   'modules/user/user.css' => FALSE,
+  //   'misc/vertical-tabs.css' => FALSE,
+  //   // Remove contrib module CSS
+  //   drupal_get_path('module', 'views') . '/css/views.css' => FALSE,
+  // );
+  // $css = array_diff_key($css, $exclude);
+}
+
 /**
 * @file
 * Contains functions to alter Drupal's markup for the <%= themeName %> theme.
